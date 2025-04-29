7e21e7e1589c7ff9ef854de75e82282924f85ffc
kima1002 <kima.serobyan1002@gmail.com>
2025-04-29 16:03:48 +0400

    Delete backend/dist directory

--- backend/dist/users/dto/update-profile.dto.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class UpdateProfileDto {
-    name?: string;
-    surname?: string;
-    bio?: string;
-    personality?: string;
-}
--- backend/dist/users/users.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { UsersService } from './users.service';
-import { UpdateProfileDto } from './dto/update-profile.dto';
-export declare class UsersController {
-    private readonly usersService;
-    constructor(usersService: UsersService);
-    getProfile(user: any): Promise<import("./user.schema").UserDocument>;
-    updateProfile(user: any, updateProfileDto: UpdateProfileDto): Promise<import("./user.schema").UserDocument>;
-}
